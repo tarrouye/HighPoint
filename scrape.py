e61186d856e3d9ea1c28016299455048b0057e92
Théo Arrouye <theo@arrouye.net>
2019-07-06 14:23:44 -0700

    Build GUI for graphing

--- scrape.py
+++ scrape.py
@@ -3,13 +3,13 @@ from scraper import StockScraper
 from portfolio import Portfolio
 
 # create a portfolio from the tesla file
-tesla = Portfolio(company_file = "teslaonly.csv", id_ = 'tsla')
+tesla = Portfolio(company_file = "companies.csv", id_ = 'tsla')
 
 # run the stock scraper
 stock = StockScraper(tesla, start = "2016-01-01", end = "2018-01-01")
 stock.scrape()
 
 # run the google scraper on the same time frame
-goog = GoogleScraper(tesla, start = "2016-01-01", end = "2018-01-01", max = 2450)
+goog = GoogleScraper(tesla, start = "2016-01-01", end = "2018-01-01", max = 50)
 goog.scrape()
 
